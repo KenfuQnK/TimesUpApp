d8cd0b257e285f3997c7903b8f20762fbed67ca0
KenfuQnK <86153113+KenfuQnK@users.noreply.github.com>
2024-11-23 13:50:43 +0100

    Create CardContent.tsx

---
+++ CardContent.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface CardContentProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const CardContent: React.FC<CardContentProps> = ({
+  children,
+  className,
+}) => {
+  return (
+    <div className={`p-4 ${className}`}>{children}</div>
+  );
+};
