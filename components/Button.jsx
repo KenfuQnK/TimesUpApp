a89ae079c33f5c37f5c99f337b26cec293807015
KenfuQnK <86153113+KenfuQnK@users.noreply.github.com>
2024-11-23 16:39:41 +0100

    Update Button.jsx

--- components/Button.jsx
+++ components/Button.jsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { cn } from "./utils"
 
 export const Button= React.forwardRef(({ className, variant, size, ...props }, ref) => {
-  const baseStyles = "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+  const baseStyles = "bg-purple-100 inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 "
   
   const variants = {
     default: "bg-primary text-primary-foreground hover:bg-primary/90",
