f6561eb523495ea9fba3c2617d0ede206725ee72
KenfuQnK <86153113+KenfuQnK@users.noreply.github.com>
2024-11-23 16:01:23 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: './postcss.config.js', // asegúrate de que Vite usa el archivo de configuración correcto
+  },
   build: {
     outDir: 'dist', // Define la carpeta de salida
   },
