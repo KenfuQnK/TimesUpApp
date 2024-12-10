3793168e372903945c16447fb7abee697f2b9c46
KenfuQnK <86153113+KenfuQnK@users.noreply.github.com>
2024-12-11 00:36:54 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { Card } from './components/Card';
 import { CardContent } from './components/CardContent';
 import { Button } from './components/Button';
 import { Input } from './components/Input';
-import { Plus, Shuffle, X, Check, Trophy, UserCircle, Play, RotateCcw, History } from 'lucide-react';
+import { Plus, Shuffle, X, Check, Trophy, UserCircle, Play, RotateCcw, History, AlertTriangle } from 'lucide-react';
 import { INITIAL_CARDS } from './LIST';
 
 // Definición de constantes globales
